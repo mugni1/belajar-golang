f0d99ec271795c77e9738d1e402a74de3e5836ac
malascoding <asepabdulmugni22@gmail.com>
2024-09-24 12:45:03 +0700

    add data array to array.go

---
+++ array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//array
+	var dataNama [3]string
+	dataNama[0] = "asep"
+	dataNama[1] = "ucep"
+	dataNama[2] = "usuf"
+
+	fmt.Println(dataNama[0])
+	fmt.Println(dataNama[2])
+	fmt.Println(dataNama[1])
+}
