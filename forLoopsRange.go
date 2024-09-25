83cf1490d38219ef30c35ac5ab38a928932b4696
malascoding <asepabdulmugni22@gmail.com>
2024-09-25 14:57:21 +0700

    add forLoopRange.go

--- forLoopsRange.go
+++ forLoopsRange.go
@@ -3,21 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	//cara 1
-	result := 1
-	for result <= 3 {
-		fmt.Println("Hallo Dunia" , result)
-		result++;
-	}
-	
-	//cara 2
-	for i := 1; i <= 3; i++ {
-		fmt.Println("Hallo World",i)
-	}
-
 	//CONTOH PENGGUNAAN
-	slice := []string{"Eko","Agus","Iki","tes"}
-	
+	slice := []string{"Eko", "Agus", "Iki", "tes"}
+
 	// FOR SLICE
 	fmt.Println("FOR RANGE UNTUK SLICE")
 	//menggunakan for range | untuk slice dan array
@@ -27,16 +15,16 @@ func main() {
 	}
 
 	//FOR MAP
-	map1 := make(map[string]string);
-	map1["title"]= "Ini title"
+	map1 := make(map[string]string)
+	map1["title"] = "Ini title"
 	map1["isi"] = "lorem ipsum dolor sit amet"
 	fmt.Println("FOR RANGE UNTUK MAP")
 	for _, v := range map1 {
 		fmt.Println(v)
 	}
-	
+
 	//FOR ARRAU
-	arr1 := [...]string{"eko","kurniawan","kanedi"}
+	arr1 := [...]string{"eko", "kurniawan", "kanedi"}
 	fmt.Println("FOR RANGE UNTUK ARRAY")
 	for _, v := range arr1 {
 		fmt.Println(v)
